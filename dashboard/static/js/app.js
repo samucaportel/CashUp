780f88c7cd9de12437bea4072aefde5ee098a19e
samucaportel <samuel@mobiconn.com.br>
2026-06-04 14:54:40 -0300

    Replace force button emoji with SVG bolt icon

--- dashboard/static/js/app.js
+++ dashboard/static/js/app.js
@@ -209,7 +209,10 @@ function createEntityCard(entity, info) {
         </div>
         <div class="entity-actions">
             <button class="btn-entity-sync" onclick="triggerSync('${entity}')">⟳ Sincronizar</button>
-            <button class="btn-entity-force" onclick="triggerSync('${entity}', true)" title="Reenviar todos os registros ignorando o ID_SINC">⚡ Forçar</button>
+            <button class="btn-entity-force" onclick="triggerSync('${entity}', true)" title="Reenviar todos os registros ignorando o ID_SINC">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="14" height="14" style="vertical-align:middle;margin-right:4px"><path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" /></svg>
+                Forçar
+            </button>
             <button class="btn-entity-history" onclick="showHistory('${entity}')" title="Histórico">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width="16" height="16">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
